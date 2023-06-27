25ccb1266b74f3460f23cab8ec0c9bde47f44706
Egor <9999966666ivan@gmail.com>
2023-06-27 13:02:21 +0300

    Добавил ввод массива пользователем

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу, которая 
+// 1. из имеющегося массива строк 
+// 2. формирует новый массив из строк, 
+// 3. длина которых меньше, либо равна 3 символам.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+
+MessageToTheUser();
+
+void MessageToTheUser()
+{
+    Console.WriteLine("Введите элементы массива через пробел:");
+    string input = Console.ReadLine();
+    string[] inputArray = input.Split(' ');
+}
+
